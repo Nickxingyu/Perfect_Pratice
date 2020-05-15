96b9cc2d5e9bfb192c1ee70f8cd70b54bee083c8
Nickxingyu <nick19960723@gmail.com>
2020-05-15 05:19:47 +0000

    change the query method

--- W2Q3.sql
+++ W2Q3.sql
@@ -1,49 +1,13 @@
 USE employees
 
 CREATE TEMPORARY TABLE dept_gender_salary
-SELECT dept_no, gender, salary
-FROM(
-	SELECT emp_no, salary
-	FROM(
-		SELECT emp_no, MAX(from_date) from_date
-		FROM salaries GROUP BY emp_no
-	)s1
-	LEFT JOIN salaries USING(emp_no, from_date)
-)s2
-INNER JOIN(
-    SELECT emp_no, dept_no
-	FROM(
-		SELECT emp_no, MAX(from_date) from_date
-		FROM dept_emp GROUP BY emp_no
-	)s3
-	LEFT JOIN dept_emp USING(emp_no, from_date)
-)s4
-INNER JOIN employees USING(emp_no);
 
+SELECT emp_no, salary
+FROM salaries s
+LEFT JOIN(
+	SELECT emp_no, MAX(from_date) last_date
+	FROM salaries GROUP BY emp_no
+) s1 USING(emp_no)
+WHERE  from_date = last_date;
 
-CREATE TEMPORARY TABLE Summary
-SELECT 'Total' dep, gender, AVG(salary) avg
-FROM dept_gender_salary
-GROUP BY gender;
-
-CREATE TEMPORARY TABLE Entire_dept_gender_salary
-SELECT * FROM Summary
-UNION
-(SELECT dept_no dep, gender, AVG(salary) avg
-FROM dept_gender_salary
-GROUP BY dep, gender);
-
-
-SELECT dep, mt.avg-ft.avg diff
-FROM (
-    SELECT * FROM Entire_dept_gender_salary WHERE gender='M'
-) mt
-INNER JOIN (
-    SELECT * FROM(
-         SELECT * FROM Summary
-         UNION
-         SELECT dept_no dep, gender, AVG(salary) avg
-         FROM dept_gender_salary
-         GROUP BY dep, gender
-    ) t WHERE gender='F'
-) ft USING(dep);
+SELECT * FROM dept_gender_salary;
