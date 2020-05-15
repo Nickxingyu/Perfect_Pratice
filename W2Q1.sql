c0fff6a9dbfb5014c01282b5b2fe25285e91158a
Nickxingyu <40970256+Nickxingyu@users.noreply.github.com>
2020-05-15 13:32:48 +0800

    finnish Q1

---
+++ W2Q1.sql
@@ -0,0 +1,10 @@
+SELECT 
+    dept_no, 
+    dept_name, 
+    AVG(salary) avg_salary
+FROM dept_emp
+INNER JOIN departments USING (dept_no)
+INNER JOIN salaries USING (emp_no)
+GROUP BY dept_name
+ORDER BY avg_salary DESC
+LIMIT 1;
