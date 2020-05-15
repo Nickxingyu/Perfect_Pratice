0fd0b89c0edb60339f8cac204b17e1466bf64c67
Nickxingyu <40970256+Nickxingyu@users.noreply.github.com>
2020-05-15 13:24:03 +0800

    Update W2Q3.sql

--- W2Q3.sql
+++ W2Q3.sql
@@ -1,13 +1,51 @@
 USE employees
 
 CREATE TEMPORARY TABLE dept_gender_salary
+SELECT dept_no, gender, salary
+FROM(
+    SELECT emp_no, salary
+    FROM salaries s
+    LEFT JOIN(
+        SELECT emp_no, MAX(from_date) last_date
+        FROM salaries GROUP BY emp_no
+    ) s1 USING(emp_no)
+    WHERE  from_date = last_date;
+)s2
+INNER JOIN(
+    SELECT emp_no, dept_no
+    FROM dept_emp
+    LEFT JOIN(
+        SELECT emp_no, MAX(from_date) last_date
+        FROM dept_emp GROUP BY emp_no
+    ) s3 USING(emp_no)
+    WHERE  from_date = last_date;
+)s4
+INNER JOIN employees USING(emp_no);
 
-SELECT emp_no, salary
-FROM salaries s
-LEFT JOIN(
-	SELECT emp_no, MAX(from_date) last_date
-	FROM salaries GROUP BY emp_no
-) s1 USING(emp_no)
-WHERE  from_date = last_date;
 
-SELECT * FROM dept_gender_salary;
+CREATE TEMPORARY TABLE Summary
+SELECT 'Total' dep, gender, AVG(salary) avg
+FROM dept_gender_salary
+GROUP BY gender;
+
+CREATE TEMPORARY TABLE Entire_dept_gender_salary
+SELECT * FROM Summary
+UNION
+(SELECT dept_no dep, gender, AVG(salary) avg
+FROM dept_gender_salary
+GROUP BY dep, gender);
+
+
+SELECT dep, mt.avg-ft.avg diff
+FROM (
+    SELECT * FROM Entire_dept_gender_salary WHERE gender='M'
+) mt
+INNER JOIN (
+    SELECT * FROM(
+         SELECT * FROM Summary
+         UNION
+         SELECT dept_no dep, gender, AVG(salary) avg
+         FROM dept_gender_salary
+         GROUP BY dep, gender
+    ) t WHERE gender='F'
+) ft USING(dep);
