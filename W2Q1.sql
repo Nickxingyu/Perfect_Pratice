3a753ac11cc34b30d4b116ff64d3d200f62a16f9
Nickxingyu <40970256+Nickxingyu@users.noreply.github.com>
2020-05-15 14:56:45 +0800

    Update W2Q1.sql

--- W2Q1.sql
+++ W2Q1.sql
@@ -1,10 +1,25 @@
 SELECT 
-    dept_no, 
-    dept_name, 
+    dept_no,  
     AVG(salary) avg_salary
-FROM dept_emp
-INNER JOIN departments USING (dept_no)
-INNER JOIN salaries USING (emp_no)
-GROUP BY dept_name
+FROM employees
+INNER JOIN(
+    SELECT emp_no, salary
+    FROM salaries s
+    LEFT JOIN(
+        SELECT emp_no, MAX(from_date) last_date
+        FROM salaries GROUP BY emp_no
+    ) s1 USING(emp_no)
+    WHERE  from_date = last_date
+)s2 USING(emp_no)
+INNER JOIN(
+    SELECT emp_no, dept_no
+    FROM dept_emp
+    LEFT JOIN(
+        SELECT emp_no, MAX(from_date) last_date
+        FROM dept_emp GROUP BY emp_no
+    ) s3 USING(emp_no)
+    WHERE  from_date = last_date
+)s4 USING(emp_no)
+GROUP BY dept_no
 ORDER BY avg_salary DESC
-LIMIT 1;
+LIMIT 1 ;
